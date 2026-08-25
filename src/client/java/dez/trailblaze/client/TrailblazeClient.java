fe8a3acda3d71fe5dcc7f6237aada5402e98c21a
dezertman1 <anthonynn5@proton.me>
2026-08-25 15:50:18 -0400

    Initial commit

---
+++ src/client/java/dez/trailblaze/client/TrailblazeClient.java
@@ -0,0 +1,10 @@
+package dez.trailblaze.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrailblazeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
