67c88ae30d29afb6195767a81cb4e71be26fa581
dezertman1 <anthonynn5@proton.me>
2026-08-25 17:16:59 -0400

    Add client-side chunk load event listener and console logging

--- src/client/java/dez/trailblaze/client/TrailblazeClient.java
+++ src/client/java/dez/trailblaze/client/TrailblazeClient.java
@@ -1,10 +1,13 @@
 package dez.trailblaze.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents;
 
 public class TrailblazeClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientChunkEvents.CHUNK_LOAD.register((world, chunk) -> {
+			System.out.println("Chunk loaded: " + chunk.getPos().x() + ", " + chunk.getPos().z());
+		});
 	}
 }
