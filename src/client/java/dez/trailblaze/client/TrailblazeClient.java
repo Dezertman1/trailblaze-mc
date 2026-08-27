e0474f373917bc903e12812432b62a22345c1fa7
dezertman1 <anthonynn5@proton.me>
2026-08-27 16:10:11 -0400

    Get matching ServerLevel and seed from chunk-load

--- src/client/java/dez/trailblaze/client/TrailblazeClient.java
+++ src/client/java/dez/trailblaze/client/TrailblazeClient.java
@@ -1,13 +1,32 @@
 package dez.trailblaze.client;
 
+import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.level.ServerLevel;
 
 public class TrailblazeClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ClientChunkEvents.CHUNK_LOAD.register((world, chunk) -> {
-			System.out.println("Chunk loaded: " + chunk.getPos().x() + ", " + chunk.getPos().z());
+			Minecraft client = Minecraft.getInstance();
+			IntegratedServer server = client.getSingleplayerServer();
+			if (server == null) return; // Not singleplayer
+
+			ServerLevel serverLevel = server.getLevel(world.dimension());
+			if (serverLevel == null) {
+				System.out.println("No matching serverLevel found");
+				return;
+			}
+
+			System.out.println("serverLevel: " + serverLevel + "seed: " + serverLevel.getSeed());
+			// System.out.println("Chunk loaded: " + chunk.getPos().x() + ", " + chunk.getPos().z());
 		});
+
+
+
+
 	}
 }
