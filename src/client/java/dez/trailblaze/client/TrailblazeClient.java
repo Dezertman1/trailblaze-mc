4642d180088034cb17bc6f8412beb89969954ee3
dezertman1 <anthonynn5@proton.me>
2026-08-27 16:19:48 -0400

    Construct ProtoChunk

--- src/client/java/dez/trailblaze/client/TrailblazeClient.java
+++ src/client/java/dez/trailblaze/client/TrailblazeClient.java
@@ -6,6 +6,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.chunk.PalettedContainerFactory;
+import net.minecraft.world.level.chunk.ProtoChunk;
+import net.minecraft.world.level.chunk.UpgradeData;
 
 public class TrailblazeClient implements ClientModInitializer {
 	@Override
@@ -16,11 +19,17 @@ public class TrailblazeClient implements ClientModInitializer {
 			if (server == null) return; // Not singleplayer
 
 			ServerLevel serverLevel = server.getLevel(world.dimension());
-			if (serverLevel == null) {
-				System.out.println("No matching serverLevel found");
-				return;
-			}
+			if (serverLevel == null) return;
 
+			ProtoChunk protoChunk = new ProtoChunk(
+					chunk.getPos(),
+					UpgradeData.EMPTY,
+					serverLevel,
+					PalettedContainerFactory.create(serverLevel.registryAccess()),
+					null
+					);
+
+			System.out.println("Created empty ProtoChunk at " + protoChunk.getPos());
 			System.out.println("serverLevel: " + serverLevel + "seed: " + serverLevel.getSeed());
 			// System.out.println("Chunk loaded: " + chunk.getPos().x() + ", " + chunk.getPos().z());
 		});
